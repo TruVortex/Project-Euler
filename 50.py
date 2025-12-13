3156e7326f35bbd43ab52ffd1bb4cf7486f85a81
TruVortex <75190116+TruVortex@users.noreply.github.com>
2025-12-12 20:01:17 -0500

    Update 50.py

--- 50.py
+++ 50.py
@@ -1,4 +1,4 @@
-ans, is_prime, primes = (-1, -1), [False] * 2 + [True] * 1000000, []
+ans, is_prime, primes = (-1, 0), [False] * 2 + [True] * 1000000, []
 for i in range(2, 1000002):
     if is_prime[i]:
         primes.append(i)
@@ -7,7 +7,9 @@ for i in range(2, 1000002):
 for i in range(1, len(primes)):
     primes[i] += primes[i - 1]
 for i in range(len(primes)):
-    for j in range(i - ans[1]):
-        if primes[i] - primes[j] < 1000000 and is_prime[primes[i] - primes[j]] and i - j + 1 > ans[1]:
+    for j in range(i - ans[1] - 1, -1, -1):
+        if primes[i] - primes[j] >= 1000000:
+            break
+        elif is_prime[primes[i] - primes[j]]:
             ans = (primes[i] - primes[j], i - j + 1)
 print(ans[0])

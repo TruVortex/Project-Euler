31765e86903d4e91d099501ee0b02114f753b348
TruVortex <75190116+TruVortex@users.noreply.github.com>
2025-12-15 09:34:54 -0500

    Update 72.py

--- 72.py
+++ 72.py
@@ -1,8 +1,8 @@
-ans, totient = 0, list(range(1000001))
+cnt, totient = 0, list(range(1000001))
 for i in range(2, 1000001):
     if totient[i] == i:
         for j in range(i, 1000001, i):
             totient[j] *= i - 1
             totient[j] //= i
-    ans += totient[i]
-print(ans)
+    cnt += totient[i]
+print(cnt)

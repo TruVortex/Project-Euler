cf5560e1c2e26239d8cfc6710c0c68e13a8a4758
TruVortex <75190116+TruVortex@users.noreply.github.com>
2025-12-14 20:01:48 -0500

    Update 71.py

--- 71.py
+++ 71.py
@@ -4,4 +4,4 @@ numer, denom = 0, 1
 while denom + 7 <= 1000000:
     numer, denom = numer + 3, denom + 7
     numer, denom = numer // gcd(numer, denom), denom // gcd(numer, denom)
-print(numer, denom)
+print(numer)
